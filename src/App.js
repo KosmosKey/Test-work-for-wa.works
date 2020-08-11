29c18b8aa442b5b97870f60bec12c74dfe83e5ba
KosmosKey <khamidovmaga2002@gmail.com>
2020-08-11 21:47:22 +0200

    Finished with the task work!

--- src/App.js
+++ src/App.js
@@ -2,20 +2,43 @@ import React from "react";
 import "./App.scss";
 import Logo from "./Image/Logo.png";
 import KeyImage from "./Image/Key.png";
+import opened from "./Image/Opened.png";
+import closed from "./Image/Closed.png";
+import SuccessChain from "./Image/SuccessChain.png";
 import { useState } from "react";
 
 const App = () => {
-  const [welcomeField, setWelcomeField] = useState(true);
-  const [selectedField, setSelectedField] = useState(2);
+  const [welcomeField, setWelcomeField] = useState(false);
+  const [selectedField, setSelectedField] = useState(1);
   const [checked, setChecked] = useState(false);
-  const [percentage, setPercentage] = useState(50);
+  const [checkedSecond, setCheckedSecond] = useState(false);
+  const [checkedThird, setCheckedThird] = useState(false);
+  const [percentage, setPercentage] = useState(0);
 
   const checkBoxValue = () => {
     setChecked(!checked);
-    if (checked === true) {
-      alert("Yes");
+    if (checked === false) {
+      setPercentage(percentage + 20);
     } else {
-      alert("lets go");
+      setPercentage(percentage - 20);
+    }
+  };
+
+  const checkBoxSecond = () => {
+    setCheckedSecond(!checkedSecond);
+    if (checkedSecond === false) {
+      setPercentage(percentage + 20);
+    } else {
+      setPercentage(percentage - 20);
+    }
+  };
+
+  const checkBoxThird = () => {
+    setCheckedThird(!checkedThird);
+    if (checkedThird === false) {
+      setPercentage(percentage + 60);
+    } else {
+      setPercentage(percentage - 60);
     }
   };
 
@@ -56,17 +79,48 @@ const App = () => {
         {selectedField === 2 && (
           <div className="App__DiscoverNextCoWorkerSection">
             <div className="App__DiscoverText">
-              <h1>Discover your next coworker</h1>
+              <h1 style={{ textAlign: "left" }}>Discover your next coworker</h1>
             </div>
             <div className="App__DiscoverInputFields">
-              <label>
-                <input
-                  type="checkbox"
-                  checked={checked}
-                  onChange={() => checkBoxValue()}
-                />
-                Check Me!
-              </label>
+              <div className="App__Labels">
+                <div className="checkbox">
+                  <input
+                    type="checkbox"
+                    id="checkbox2"
+                    checked={checked}
+                    onChange={() => checkBoxValue()}
+                  />
+                  <label for="checkbox2">
+                    <span>Upload logo</span>
+                  </label>
+                </div>
+
+                <div className="checkboxSecond">
+                  <input
+                    type="checkbox"
+                    className="CheckBox__Second"
+                    id="checkbox3"
+                    checked={checkedSecond}
+                    onChange={() => checkBoxSecond()}
+                  />
+                  <label for="checkbox3">
+                    <span>Add description</span>
+                  </label>
+                </div>
+
+                <div className="checkboxThird">
+                  <input
+                    type="checkbox"
+                    id="checkbox4"
+                    checked={checkedThird}
+                    onChange={() => checkBoxThird()}
+                  />
+                  <label for="checkbox4">
+                    <span>Upgrade</span>
+                  </label>
+                </div>
+              </div>
+
               <div className="App__LoadingBar">
                 <div className="App__KeyImages">
                   <img
@@ -76,13 +130,13 @@ const App = () => {
                   />
                   <img
                     src={KeyImage}
-                    alt="KeyImageOne"
+                    alt="KeyImageTwo"
                     className="KeySecond__"
                   />
 
                   <img
                     src={KeyImage}
-                    alt="KeyImageOne"
+                    alt="KeyImageThree"
                     className="KeyThird__"
                   />
                 </div>
@@ -94,7 +148,84 @@ const App = () => {
                 </div>
               </div>
             </div>
+            <div className="App__CloseIcons">
+              <div
+                className="App__CloseIconsText"
+                style={{ marginRight: "50px" }}
+              >
+                {checked ? (
+                  <img
+                    onClick={() => setSelectedField(3)}
+                    src={opened}
+                    alt="closedOpenFirst"
+                  />
+                ) : (
+                  <img src={closed} alt="closedOpenFirst" />
+                )}
+                <p>
+                  Free interviews
+                  <br /> Pay per hire
+                </p>
+              </div>
+              <div className="App__CloseIconsText">
+                {checkedSecond ? (
+                  <img
+                    src={opened}
+                    onClick={() => setSelectedField(3)}
+                    alt="closedOpenFirst"
+                  />
+                ) : (
+                  <img src={closed} alt="closedOpenFirst" />
+                )}
+                <p>
+                  Free interviews
+                  <br /> Pay per hire
+                </p>
+              </div>
+              <div
+                className="App__CloseIconsText"
+                style={{ marginLeft: "50px" }}
+              >
+                {checkedThird ? (
+                  <img
+                    src={opened}
+                    onClick={() => setSelectedField(3)}
+                    alt="closedOpenFirst"
+                  />
+                ) : (
+                  <img src={closed} alt="closedOpenFirst" />
+                )}
+                <p>
+                  Free interviews
+                  <br /> Pay per hire
+                </p>
+              </div>
+            </div>
           </div>
+        )}
+
+        {selectedField === 3 ? (
+          <div className="App__SuccessMessageChain">
+            <div className="App__ChainNav">
+              <p onClick={() => setSelectedField(2)}>Back</p>
+              <p onClick={() => setWelcomeField(!welcomeField)}>Hide</p>
+            </div>
+            <div className="App_chainImageAndText">
+              <div className="App__SectionSuccess">
+                <img src={SuccessChain} alt="SuccessChain" />
+              </div>
+              <div className="App_ChainText">
+                <h2>Juhuu, you're ready for pokes!</h2>
+                <p>
+                  Pokes are candidate's way of showing <br />
+                  interest in your workplace
+                </p>
+                <button>FIND CANDIDATES</button>
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
         )}
       </div>
       <button
